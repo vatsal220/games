40ca8c9e234c5a412905e463c7713ca15a1c02df
vatsal220 <vatsal.p1997@gmail.com>
2021-06-26 03:27:38 -0400

    created initial script for sudoku

--- games/__init__.py
+++ games/__init__.py
@@ -1,2 +1,3 @@
 from games.tictactoe.ttt import TicTacToe
 from games.rockpaperscizors.rps import RockPaperScissors
+from games.sudoku import Sudoku
---
+++ games/sudoku/sudoku.py
@@ -0,0 +1,25 @@
+import numpy as np
+import random
+
+class Sudoku():
+    def __init__(self, grid_size):
+        self.grid_size = grid_size
+
+    def generate_board(self):
+        pass
+
+    def generate_solution(self):
+        pass
+
+    def remove_elements(self):
+        pass
+
+    def is_valid_solution(self):
+        pass
+
+    def play(self):
+        pass
+
+if __name__ == '__main__':
+    sd = Sudoku(grid_size = 9)
+    print('here')
