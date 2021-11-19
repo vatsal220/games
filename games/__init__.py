4c310694be3082a4549715949e19427e008c0735
vatsal220 <vatsal.p1997@gmail.com>
2021-11-18 23:39:55 -0500

    Added Typing class into the init script

--- games/__init__.py
+++ games/__init__.py
@@ -4,4 +4,4 @@ from games.sudoku.sudoku import Sudoku
 from games.snake.snake import Snake
 from games.connect4.c4 import ConnectFour
 from games.minesweeper.ms import MineSweeper
-from games.typing_test.type import * 
+from games.typing_test.type import Typing 
