23901ad62941a2c4ab73584ec9c71dfd000dab64
vatsal220 <vatsal.p1997@gmail.com>
2022-01-22 23:48:13 -0500

    added function to generate the board

--- games/snake/snake.py
+++ games/snake/snake.py
@@ -5,10 +5,21 @@ from curses import wrapper
 
 class Snake():
     def __init__(self, grid_size):
+        '''
+        params:
+            grid_size (List) : The list holds the number the rows and columns
+        '''
         self.grid_size = grid_size
 
     def generate_map(self):
-        pass
+        '''
+        This function will generate the map for the snake to move on
+        '''
+        try:
+            map = np.zeros(grid_size)
+            return map
+        except:
+            raise ValueError("Please enter valid grid size dimensions")
 
     def move_user(self):
         pass
